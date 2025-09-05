360f147c1b91fece208bc8093cbc8a2cfc113f56
ygorrr <ygor.ares@gmail.com>
2025-09-05 20:07:14 -0300

    cobem2025

--- k-epsilon/1d-transient/cobem-1d.jl
+++ k-epsilon/1d-transient/cobem-1d.jl
@@ -7,72 +7,72 @@ using LineSearches: BackTracking
 # -------------------------------------------------------------------
 
 # Parâmetros geométricos
-const reactorDiam = 1.8 # m
-const reactorHeight = 2 * reactorDiam # m
-const reactorCrossArea = π*reactorDiam^2/4
-const hdt = 1*60*60 # s
-const reactorVelocity = reactorHeight/hdt # m/s
+φ_uasb = 1.8 # m
+H_uasb = 2 # m
+A_uasb = π*(φ_uasb/2)^2
+Q = 0.5 / 1000 # m³/s
+HDT = H_uasb * A_uasb / Q # s
+HDT_horas = HDT / 3600
+U_uasb = H_uasb/HDT # m/s
 
-const jetDiam = 0.2 # m
-const nJets = 1
-const totalJetArea = (π*jetDiam^2/4)*nJets
-const totalJetPerimeter = (π*jetDiam)*nJets
-const jetVelocity = reactorVelocity * (reactorCrossArea/totalJetArea) # m/s
+φ_jet = 0.2 # m
+A_jet = π*(φ_jet/2)^2 # m²
+U_jet = Q/(9*A_jet) # m/s (9 orifícios por UASB)
+nJets = 1
 
 # Parâmetros físicos
-const gravityAccel = 9.81 # m/s²
-const fluidSpecificMass = 997.0 # kg/m³
-const fluidDynamicViscosity = 0.00089 # Pa*s
-const fluidKinematicViscosity = fluidDynamicViscosity/fluidSpecificMass
-const molecularDiffC = 1.0 # m²/s
-const beta = 0.1
+g = 9.81 # m/s²
+ρ = 1000.0 # kg/m³
+μ = 1.0e-3 # Pa*s
+β = 0.1
 
 # Constantes do modelo k-epsilon
-const Cμ = 0.09
-const Cϵ1 = 1.44
-const Cϵ2 = 1.92
-const σ = 1.0
-const σk = 1.0
-const σϵ = 1.3
+Cμ = 0.09
+Cϵ1 = 1.44
+Cϵ2 = 1.92
+σ = 1.0
+σk = 1.0
+σϵ = 1.3
 
 # Grandezas características
-const URef = jetVelocity
-const LRef = jetDiam
-const CRef = 1
+Uc = U_jet
+Lc = φ_jet
+Cc = 1
 
 # Grupos adimensionais
-# const Re = cVelocity*cLength/fluidKinematicViscosity
-const Re = 300
+Re = ρ*U_jet*φ_jet/μ
+# Re = 300
 # const Sc = fluidKinematicViscosity/molecularDiffC
-const Sc = 1.0
+Sc = 1.0
 # const Fr = cVelocity/sqrt(9.81*cLength)
 # const Ri = (1/Fr^2)*cConcentration*beta
-const Ri = 0.1
+Ri = 0.1
 
-U = VectorValue(reactorVelocity / URef)
-Us = VectorValue(-0.1)
+
+U = VectorValue(0.33) # Valor adimensional calculado do perfil do jato no código 2d
+Us = VectorValue(-0.4)
 W = U + Us
 ĝ = VectorValue(-1.0)
 
-eddyVisc(k, ϵ) = Cμ*k*k/max(ϵ, 0.01)
+nu_T(k, ϵ) = Cμ*k*k/max(ϵ, 0.01)
 
 resC(t, C, k, ϵ, v1) =
   ∫( v1 * ∂t(C) )dΩ +
   ∫( v1 * W⋅∇(C) )dΩ +
-  ∫( (1/Re/Sc + (eddyVisc∘(k,ϵ)) / σ) * ∇(v1)⋅∇(C) )dΩ
+  ∫( (1/Re/Sc + (nu_T∘(k,ϵ)) / σ) * ∇(v1)⋅∇(C) )dΩ
 
 resk(t, k, ϵ, v2) =
   ∫( v2 * ∂t(k) )dΩ +
   ∫( v2 * U⋅∇(k) )dΩ +
-  ∫( (eddyVisc∘(k,ϵ)) / σk * ∇(v2)⋅∇(k) )dΩ +
-  ∫( v2 * ϵ * (tanh∘(10.0*k/dirichlet_k)) )dΩ +
-  ∫( v2 * (Ri * (eddyVisc∘(k,ϵ))/σ) * (tanh∘(10.0*k/dirichlet_k)) )dΩ
+  ∫( (nu_T∘(k,ϵ)) / σk * ∇(v2)⋅∇(k) )dΩ +
+  ∫( v2 * ϵ * (tanh∘(10.0*k/k_ΓD)) )dΩ +
+  ∫( v2 * (Ri * (nu_T∘(k,ϵ))/σ) * (tanh∘(10.0*k/k_ΓD)) )dΩ
 
 resEpsilon(t, k, ϵ, v3) =
   ∫( v3 * ∂t(ϵ) )dΩ +
   ∫( v3 * U⋅∇(ϵ) )dΩ + 
-  ∫( (eddyVisc∘(k, ϵ))/σϵ * ∇(v3)⋅∇(ϵ) )dΩ +
-  ∫( v3 * Cϵ2 * ϵ * ϵ/k * (tanh∘(10.0*ϵ/dirichlet_epsilon)) )dΩ
+  ∫( (nu_T∘(k, ϵ))/σϵ * ∇(v3)⋅∇(ϵ) )dΩ +
+  ∫( v3 * Cϵ2 * ϵ * ϵ/k * (tanh∘(10.0*ϵ/ϵ_ΓD)) )dΩ
 
 res(t, (C, k, ϵ), (v1, v2, v3)) =  
   resC(t, C, k, ϵ, v1) +
@@ -84,7 +84,7 @@ res(t, (C, k, ϵ), (v1, v2, v3)) =
 # -------------------------------------------------------------------
 
 n = 150
-model_Ω = CartesianDiscreteModel((0, reactorHeight/LRef), (n))
+model_Ω = CartesianDiscreteModel((0, H_uasb/Lc), (n))
 Ω = Interior(model_Ω)
 dΩ = Measure(Ω, 2)
 
@@ -92,21 +92,22 @@ dΩ = Measure(Ω, 2)
 refFE = ReferenceFE(lagrangian, Float64, 1)
 
 # Espaços de funções de teste
-testSpace_C = TestFESpace(model_Ω, refFE, conformity=:H1, dirichlet_tags="boundary")
-testSpace_k = TestFESpace(model_Ω, refFE, conformity=:H1, dirichlet_tags="boundary")
-testSpace_epsilon = TestFESpace(model_Ω, refFE, conformity=:H1, dirichlet_tags="boundary")
+testSpace_C = TestFESpace(model_Ω, refFE, conformity=:H1, dirichlet_tags="tag_1")
+testSpace_k = TestFESpace(model_Ω, refFE, conformity=:H1, dirichlet_tags="tag_1")
+testSpace_epsilon = TestFESpace(model_Ω, refFE, conformity=:H1, dirichlet_tags="tag_1")
 
 # Condições de contorno
-bc_C(x, t::Real) = (1 - LRef/reactorHeight*x[1])
+bc_C(x, t::Real) = (1 - Lc/H_uasb*x[1])
 bc_C(t::Real) = x -> bc_C(x, t)
 
 I = 0.1
-dirichlet_k = (3/2)*(I*jetVelocity/URef)^2 #(1/2)*(reactorCrossArea/totalJetArea)^2
-bc_k(x, t::Real) = dirichlet_k*(1 - LRef/reactorHeight*x[1])
+k_ΓD = (3/2)*(I*U_jet/Uc)^2
+bc_k(x, t::Real) = k_ΓD*(1 - Lc/H_uasb*x[1])
 bc_k(t::Real) = x -> bc_k(x, t)
 
-dirichlet_epsilon = Cμ*dirichlet_k/(0.01*jetVelocity*jetDiam/URef/LRef) #2*(reactorDiam/jetDiam)*(reactorCrossArea/totalJetArea)^3
-bc_epsilon(x, t::Real) = dirichlet_epsilon*(1 - LRef/reactorHeight*x[1])
+γ = 0.07
+ϵ_ΓD = Cμ^(3/4) * k_ΓD^(3/2) / (γ * φ_jet/Lc)
+bc_epsilon(x, t::Real) = ϵ_ΓD * (1 - Lc/H_uasb*x[1])
 bc_epsilon(t::Real) = x -> bc_epsilon(x, t)
 
 # Espaços de funções candidatas
@@ -122,7 +123,7 @@ op = TransientFEOperator(res, X, Y)
 nls = NLSolver(show_trace=true, method=:newton, linesearch=BackTracking(), iterations=10)
 
 CFL = 0.25
-dx = reactorHeight/LRef/n
+dx = H_uasb/Lc/n
 Δt = CFL*dx
 θ = 1
 ode_solver = ThetaMethod(nls,Δt,θ)
@@ -146,7 +147,7 @@ Case description
 ----------------------------
 Spatial dimensions: 1
 Transient:          true
-Reynolds number:    $ReStr
+Reynolds number:    $Re
 Richardson number:  $Ri
 Number of jets:     $nJets
 Settling velocity:  $Us
@@ -166,16 +167,16 @@ end
 touch(path)
 
 path = joinpath(@__DIR__, "output")
-if !isdir(dirPath)
-  mkdir(dirPath)
+if !isdir(path)
+  mkdir(path)
 end
 
 ReStr = Int(round(Re, RoundDown))
 path = joinpath(path,"Re$ReStr-nJets$nJets")
-if isdir(dirPath)
-  rm(dirPath, recursive=true)
+if isdir(path)
+  rm(path, recursive=true)
 end
-mkdir(dirPath)
+mkdir(path)
 
 #=------------------------
   Solução e escrita dos resultados
@@ -184,7 +185,7 @@ mkdir(dirPath)
 ch, kh, epsilonh = u_θ
 it = 0
 
-writevtk(Ω,(@__DIR__)*"/output/Re$ReStr-nJets$nJets/uasbcp$it.vtu",cellfields=["ch"=>ch, "kh"=>kh, "epsilonh"=>epsilonh, "nuT"=>eddyVisc∘(kh, epsilonh)])
+writevtk(Ω,(@__DIR__)*"/output/Re$ReStr-nJets$nJets/uasbcp$it.vtu",cellfields=["ch"=>ch, "kh"=>kh, "epsilonh"=>epsilonh, "nu_T"=>nu_T∘(kh, epsilonh)])
 
 totalIts = Int(round(T/Δt, RoundUp))
 for (t,u_h) in u_ht
@@ -194,8 +195,6 @@ for (t,u_h) in u_ht
   println("Iterations completed: $it/$totalIts")
   ch, kh, epsilonh = u_h 
   if (mod(it,1)==0)
-    writevtk(Ω,(@__DIR__)*"/output/Re$ReStr-nJets$nJets/uasbcp$it.vtu",cellfields=["ch"=>ch, "kh"=>kh, "epsilonh"=>epsilonh, "nuT"=>eddyVisc∘(kh, epsilonh)])
+    writevtk(Ω,(@__DIR__)*"/output/Re$ReStr-nJets$nJets/uasbcp$it.vtu",cellfields=["ch"=>ch, "kh"=>kh, "epsilonh"=>epsilonh, "nu_T"=>nu_T∘(kh, epsilonh)])
   end
 end
-
-println("Simulation completed without errors.")
